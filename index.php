ff9f72524997062569098541ba9f51a5707cd1f0
BeautyButterfl <sheanne_cruz@rcc.edu.ph>
2024-03-09 14:15:32 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <h1>Sheanne Cruz</h1>
+ 
+<body>
+<?php
+<p>Addtion</p>
+$x = 20;  
+$x += 100;
+
+echo $x;
+
+$x = 50;
+$x -= 30;
+
+echo $x;
+
+$x = 5;
+$x *= 6;
+
+echo $x;
+
+$x = 10;
+$x /= 5;
+
+echo $x;
+
+$x = 15;
+$x %= 4;
+
+echo $x;
+?>
+</head>
+<h2></h2>
+</body>
+</html>
